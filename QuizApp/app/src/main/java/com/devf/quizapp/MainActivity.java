ca26fbca87fb77ac8813f4fa66496b172df89910
Condesa <condesa89@gmail.com>
2016-02-15 17:48:45 -0600

    Se agrega método validatePosition para evitar IndexBoundException

--- QuizApp/app/src/main/java/com/devf/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/devf/quizapp/MainActivity.java
@@ -6,7 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.devf.quizapp.model.TrueFalse;
@@ -19,8 +19,16 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    @Bind(R.id.coordinator_layout) CoordinatorLayout coordinatorLayout;
-    @Bind(R.id.label_question) TextView labelQuestion;
+    @Bind(R.id.coordinator_layout)
+    CoordinatorLayout coordinatorLayout;
+    @Bind(R.id.label_question)
+    TextView labelQuestion;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.button_previous)
+    Button buttonPrevious;
+    @Bind(R.id.button_next)
+    Button buttonNext;
 
     private TrueFalse[] questionBank = new TrueFalse[]{
             new TrueFalse(R.string.question_oceans, true),
@@ -37,8 +45,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        updateQuestion();
-
+        validatePosition();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -46,10 +53,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @OnClick({R.id.button_next, R.id.label_question})
-    public void showNextQuestion(){
-        currentIndex = currentIndex+1;
-        Log.i(TAG, "Current index " + currentIndex);
-        updateQuestion();
+    public void showNextQuestion() {
+        currentIndex = currentIndex + 1;
+        validatePosition();
+    }
+
+    @OnClick(R.id.button_previous)
+    public void showPreviousQuestion() {
+        currentIndex = currentIndex - 1;
+        validatePosition();
     }
 
     @OnClick(R.id.button_true)
@@ -62,18 +74,31 @@ public class MainActivity extends AppCompatActivity {
         checkAnswer(false);
     }
 
-    private void updateQuestion(){
+    private void validatePosition(){
+        Log.i(TAG, "Current index " + currentIndex);
+        if (currentIndex == 0) {
+            buttonPrevious.setEnabled(false);
+        } else if(currentIndex == questionBank.length-1){
+            buttonNext.setEnabled(false);
+        } else {
+            buttonNext.setEnabled(true);
+            buttonPrevious.setEnabled(true);
+        }
+        updateQuestion();
+    }
+
+    private void updateQuestion() {
         int question = questionBank[currentIndex].getQuestion();
         labelQuestion.setText(question);
     }
 
-    private void checkAnswer(boolean userAnswer){
+    private void checkAnswer(boolean userAnswer) {
         boolean answerIsTrue = questionBank[currentIndex].isTrueQuestion();
         int messageResId = 0;
 
-        if(userAnswer == answerIsTrue){
+        if (userAnswer == answerIsTrue) {
             messageResId = R.string.label_correct_answer;
-        }else{
+        } else {
             messageResId = R.string.label_incorrect_answer;
         }
         Snackbar.make(coordinatorLayout, messageResId, Snackbar.LENGTH_LONG).show();
